948a9090c65cd3dfdc0534a2255849d58c093d53
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-28 11:00:18 +0530

    qcp1

---
+++ Quiz Cards/QuizCardPlayer.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import javafx.scene.control.MenuBar;
+
+import java.awt.*;
+import java.io.*;
+
+public class QuizCardPlayer
+{
+    private JTextArea display;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private QuizCard currentCard;
+    private int currentCardIndex;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
+    public static void main(String[] args) {
+        QuizCardPlayer reader = new QuizCardPlayer();
+        reader.go();
+    }
+
+    public void go()
+    {
+        frame = new JFrame("Quiz Card Player");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+
+        display = new JTextArea(10,20);
+        display.setFont(bigFont);
+
+        display.setLineWrap(true);
+        display.setEditable(false);
+
+        JScrollPane qScrollPane = new JScrollPane(display);
+        qScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        nextButton = new JButton("Show Question");
+        mainPanel.add(qScrollPane);
+        mainPanel.add(nextButton);
+        nextButton.addActionListener(new NextCardListener());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem loadMenuItem = new JMenuItem("Load card set");
+        loadMenuItem.addActionListener(new OpenMenuListener());
+        fileMenu.add(loadMenuItem);
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(640,500);
+        frame.setVisible(true);
+    }
+}
